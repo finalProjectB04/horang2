cfed9fec24031d20636ddf4356a85860b4548b37
yeeunseo-dev <anfrhrl96@gmail.com>
2024-07-23 23:29:19 +0900

    Refactor: 불필요한 주석제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: `http://apis.data.go.kr/B551011/KorService/:path*`,
-  //     },
-  //   ];
-  // },
-};
+const nextConfig = {};
 
 export default nextConfig;
