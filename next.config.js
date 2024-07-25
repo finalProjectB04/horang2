071032eb6ea7d6165123584570298fff8f24c318
youwoon <youwoon5520@gmail.com>
2024-07-25 17:54:57 +0900

    [Feat#2] fix: 코드 오류 수정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["crjcsxutfsroqsqumefz.supabase.co"], // 여기에 허용할 도메인 추가
+    domains: ["crjcsxutfsroqsqumefz.supabase.co"],
   },
 };
 
