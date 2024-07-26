899ddc58e5488a8f2f5ab6f37f61172e72d9e229
DAEWOOYANG0310 <ydw8088@naver.com>
2024-07-26 09:24:52 +0900

    Feat:메인 주석 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Control from "@/components/main/Control";
 import { MainImage } from "@/components/main/image/MainImage";
 import { TravelCourse } from "@/components/main/TravelCourse";
 import { Leports } from "@/components/main/Leports";
-
+//MainImage 검색오류입니다 신경 안쓰셔도괜찮습니다
 const Home = () => {
   return (
     <div>
