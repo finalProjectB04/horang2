080b6852e2d3f345855acd2bbfe7001d29b912d6
DAEWOOYANG0310 <ydw8088@naver.com>
2024-07-29 15:33:19 +0900

    Design:이미지 크기 고정 1920 900 으로 맞춤

--- src/components/main/image/MainImage.tsx
+++ src/components/main/image/MainImage.tsx
@@ -8,8 +8,8 @@ interface SearchBarProps {
 
 export const MainImage: React.FC<SearchBarProps> = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className="mb-6 relative w-[1920] h-[900]">
-      <Image src="/assets/images/ex6.png" alt="background" width={1920} height={900} className="w-full h-auto" />
+    <div className="relative w-[1920px] h-[900px]">
+      <Image src="/assets/images/ex6.png" alt="background" layout="fill" objectFit="cover" className="object-cover" />
       <div className="absolute bottom-10 left-0 w-full px-5 md:px-8 lg:px-10">
         <div className="max-w-6xl mx-auto my-36">
           <div className="relative w-full">
