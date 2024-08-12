539f79d917d937463ed670cad5a911d9def3f158
youwoon <youwoon5520@gmail.com>
2024-08-12 12:01:45 +0900

    [Feat#2] feat: 로그인 및 회원가입 모바일 완성

--- src/components/common/userspage/signinpage/PasswordInput.tsx
+++ src/components/common/userspage/signinpage/PasswordInput.tsx
@@ -13,7 +13,7 @@ const PasswordInput: React.FC<PasswordInputProps> = ({ password, setPassword }) 
       type="password"
       value={password}
       onChange={(event) => setPassword(event.target.value)}
-      className="mt-1 block w-full max-w-full sm:max-w-[280px] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      className="mt-1 block w-full sm:max-w-[280px] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
       required
     />
   </div>
