e090b27499ae23e66409655fc885a5161c51d8d2
RingKim1 <kgh9393@naver.com>
2024-08-12 14:00:24 +0900

    마이페이지 마진값 조정

--- src/components/common/Advertisement.tsx
+++ src/components/common/Advertisement.tsx
@@ -5,7 +5,7 @@ import banner2 from "../../../public/assets/images/banner2.png";
 
 const Advertisement: React.FC = () => {
   return (
-    <section className="my-[100px]">
+    <section className="sm:my-[20px] md:my-[50px] lg:my-[100px]">
       <Image className="sm:block lg:hidden" src={banner2} alt={"banner2"} width={375} height={140} />
       <Image className="sm:hidden lg:block" src={banner} alt={"banner"} width={1920} height={360} />
     </section>
