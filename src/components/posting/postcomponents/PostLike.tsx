1aa3fed7f0c08037cec83ff7c426c8bf1c70b4d8
DAEWOOYANG0310 <ydw8088@naver.com>
2024-08-12 10:08:15 +0900

    Design: 하트 마진

--- src/components/posting/postcomponents/PostLike.tsx
+++ src/components/posting/postcomponents/PostLike.tsx
@@ -6,7 +6,9 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 
 const DEFAULT_HEART = <Image src="/assets/images/defaultLikeIcon.png" alt="빈하트" width={48} height={48} />;
-const PUSHED_HEART = <Image src="/assets/images/successLikeIcon.png" alt="하트" width={48} height={48} />;
+const PUSHED_HEART = (
+  <Image src="/assets/images/successLikeIcon.png" alt="하트" width={48} height={48} className="mt-4" />
+);
 
 interface PostLikeProps {
   post_id: string;
