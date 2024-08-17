20f49c318c692dfe74edd8c3fab5487e32471326
[Github <helgo1318@gmail.com>
2024-08-17 22:24:04 +0900

    fix: 메인 카테고리 탭 에러 롤백

--- src/components/maindetail/mainDetailMobile/TourismList.tsx
+++ src/components/maindetail/mainDetailMobile/TourismList.tsx
@@ -7,7 +7,6 @@ import { SearchBar } from "@/components/maindetail/SearchBar";
 import { TravelCard } from "@/components/maindetail/TravelCard";
 import { ApiInformation } from "@/types/Main";
 import { useUserStore } from "@/zustand/userStore";
-
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useMemo, useState } from "react";
 import { useInView } from "react-intersection-observer";
@@ -16,11 +15,6 @@ import { useInView } from "react-intersection-observer";
 interface TourismListProps {
   contentTypeId: number;
   title: string;
-  contentId: string;
-  imageUrl: string;
-  addr1: string;
-  tel: string;
-  user_id: string;
 }
 
 // 관광 데이터를 가져오는 함수
@@ -37,7 +31,6 @@ export const TourismList: React.FC<TourismListProps> = ({ contentTypeId, title }
   const [searchTerm, setSearchTerm] = useState<string>("");
   const { ref, inView } = useInView();
   const { id: userId } = useUserStore();
-
   // React Query를 사용하여 데이터 fetching
   const {
     data: tourismData,
@@ -77,65 +70,33 @@ export const TourismList: React.FC<TourismListProps> = ({ contentTypeId, title }
 
   return (
     <>
-      <div className=" hidden lg:block">
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <div className="mx-auto py-8 max-w-[1440px] flex flex-col gap-10">
-          <div className="flex my-6 gap-3">
-            <DetailTitle />
-            <h3 className="text-2xl font-bold">{title}</h3>
-          </div>
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10py-8 max-w-[1440px]">
-            {displayedData.map((item) => (
-              <TravelCard
-                key={item.contentid}
-                item={item}
-                user_id={userId || ""}
-                imageUrl={item.firstimage || ""}
-                contentTypeId={item.contenttypeid || ""}
-                title={item.title || ""}
-                addr1={item.addr1 || ""}
-                tel={item.tel || ""}
-                contentId={item.contentid || ""}
-              />
-            ))}
-          </div>
-          {displayedData.length < filteredData.length && (
-            <div ref={ref} className="py-4 text-center">
-              Loading more...
-            </div>
-          )}
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="mx-auto py-8 max-w-[1440px] flex flex-col gap-10">
+        <div className="flex my-6 gap-3">
+          <DetailTitle />
+          <h3 className="text-2xl font-bold">{title}</h3>
         </div>
-      </div>
-      <div className=" block  lg:block">
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <div className="mx-auto py-8 max-w-[327px] flex flex-col gap-10">
-          <div className="flex my-6 gap-3">
-            <DetailTitle />
-            <h3 className="text-2xl font-bold">{title}</h3>
-          </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10py-8 max-w-[1440px]">
-            {displayedData.map((item) => (
-              <TravelCard
-                key={item.contentid}
-                item={item}
-                user_id={userId || ""}
-                imageUrl={item.firstimage || ""}
-                contentTypeId={item.contenttypeid || ""}
-                title={item.title || ""}
-                addr1={item.addr1 || ""}
-                tel={item.tel || ""}
-                contentId={item.contentid || ""}
-              />
-            ))}
-          </div>
-          {displayedData.length < filteredData.length && (
-            <div ref={ref} className="py-4 text-center">
-              Loading more...
-            </div>
-          )}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10py-8 max-w-[1440px]">
+          {displayedData.map((item) => (
+            <TravelCard
+              key={item.contentid}
+              item={item}
+              user_id={userId || ""}
+              imageUrl={item.firstimage || ""}
+              contentTypeId={item.contenttypeid || ""}
+              title={item.title || ""}
+              addr1={item.addr1 || ""}
+              tel={item.tel || ""}
+              contentId={""}
+            />
+          ))}
         </div>
+        {displayedData.length < filteredData.length && (
+          <div ref={ref} className="py-4 text-center">
+            Loading more...
+          </div>
+        )}
       </div>
     </>
   );
