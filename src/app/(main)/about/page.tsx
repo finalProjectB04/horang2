dc5654a7031ca5a7ed2821791a3aada8392dad29
[Github <helgo1318@gmail.com>
2024-08-21 07:31:46 +0900

    style: 소개 페이지 이미지 수정

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -39,7 +39,7 @@ const AboutPage = () => {
 
   const slicedData = data.slice(0, ITEMS_PER_PAGE);
   return (
-    <main className=" w-auto h-auto justify-items-center">
+    <main className=" w-auto h-auto justify-items-center min-w-[300px]">
       <section className=" w-auto h-auto bg-secondary-700 justify-items-center">
         <Image src="/assets/images/about/intro_main.png" alt={"3d타이포 몸체"} width={1920} height={1152} />
       </section>
@@ -51,13 +51,13 @@ const AboutPage = () => {
           <Image src="/assets/images/about/Vector.svg" alt={"Vector"} width={960} height={105} />
           <Image src="/assets/images/about/Vector.svg" alt={"Vector"} width={960} height={105} />
         </div>
-        <div className="ml-[241px] pt-[105px]">
-          <div className="flex items-center space-x-4">
-            <Image src="/assets/images/about/text1.svg" alt={"text1.svg"} width={259} height={24} />
-            <Image src="/assets/images/about/Horang.svg" alt={"Horang.svg"} width={132} height={35} />
-          </div>
-          <div className="mt-[24px] w-[calc(259px+132px+16px)]" style={{ borderTop: "1px solid #B2A374" }}></div>
-          <p className="mt-[24px] text-white text-[24px] font-normal">호랑과 함께 국내여행을 진행해 보세요</p>
+        <div className="w-auto h-auto flex mt-[40px]">
+          <Image
+            src="/assets/images/about/intro_under_typo.svg"
+            alt={"intro_under_typo.svg"}
+            width={1920}
+            height={248}
+          />
         </div>
         <div className="w-auto h-auto flex">
           <Image src="/assets/images/about/composition1.png" alt={"composition1.png"} width={1920} height={812} />
@@ -68,35 +68,15 @@ const AboutPage = () => {
         <div className="flex w-auto h-auto flex">
           <Image src="/assets/images/about/composition3.svg" alt={"composition3.svg"} width={1920} height={668} />
         </div>
-        <div className="flex ml-[241px] pt-[100px]">
+        <div className="flex ml-[241px] w-auto h-auto pt-[100px]">
           <div>
             <Image
-              src="/assets/images/about/my_trip_around_text.svg"
-              alt={"my_trip_around_text.svg"}
-              width={178}
-              height={29}
+              src="/assets/images/about/trip_around_text.svg"
+              alt={"trip_around_text.svg"}
+              width={1438}
+              height={128}
               className="mb-[24px]"
             />
-            <div className="w-[calc(364px)]" style={{ borderTop: "1px solid #B2A374" }}></div>
-            <p
-              className="text-white text-[24px] font-bold mt-[24px]"
-              style={{ lineHeight: "30px", letterSpacing: "-0.6px" }}
-            >
-              지금 내 주변에서 숨겨진 핫플레이스를 찾아보세요! 🌟
-            </p>
-            <p className="text-white text-[24px] font-normal" style={{ lineHeight: "30px", letterSpacing: "-0.6px" }}>
-              GPS로 내 위치를 확인하고, 20km 이내의 멋진 관광지를 탐험해보세요!
-            </p>
-            <p className="text-white text-[24px] font-normal" style={{ lineHeight: "30px", letterSpacing: "-0.6px" }}>
-              지도를 마음껏 탐색하며 새로운 여행지를 발견하고, 관광지 마커를 클릭하면 그곳의 매력적인 정보를 한눈에
-              확인할 수 있습니다.
-            </p>
-            <p
-              className="text-white text-[24px] font-normal mb-[20px]"
-              style={{ lineHeight: "30px", letterSpacing: "-0.6px" }}
-            >
-              어디에 숨겨져 있는지 모르는 매력적인 핫플레이스들을 발견하고, 내 주변에서 새로운 모험을 시작해보세요! ✨
-            </p>
           </div>
         </div>
         <div className="flex flex-col items-center justify-center mb-[100px]">
@@ -106,7 +86,11 @@ const AboutPage = () => {
 
           <div className="flex items-center justify-center">
             <Link href="/location">
-              <button className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-lg rounded-3xl">
+              <button
+                className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-[16px] rounded-3xl 
+                     sm:w-[374px] sm:h-[36px] sm:text-[11px] 
+                     md:w-[468px] md:h-[45px] md:text-[13px]"
+              >
                 내 근처 여행지 찾아보기
               </button>
             </Link>
@@ -131,9 +115,13 @@ const AboutPage = () => {
         <div className="w-full max-w-[1440px]">
           <PostGrid posts={slicedData} selectedCategory={selectedCategory} sortOrder={sortOrder} />
         </div>
-        <div className="mt-[34px] mb-[667px]">
+        <div className="mt-[34px] mb-[667px] flex items-center justify-center">
           <Link href="/community">
-            <button className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-lg rounded-3xl">
+            <button
+              className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-[24px] rounded-3xl 
+                     sm:w-[374px] sm:h-[36px] sm:text-[16px] 
+                     md:w-[468px] md:h-[45px] md:text-[20px]"
+            >
               커뮤니티 바로가기
             </button>
           </Link>
