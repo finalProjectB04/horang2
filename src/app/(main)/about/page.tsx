79a40a0be600476ccfd3aaef2123b077a748a69b
[Github <helgo1318@gmail.com>
2024-09-11 16:56:01 +0900

    fix:소개페이지 중간저장2

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -43,38 +43,126 @@ const AboutPage = () => {
 
   const slicedData = filteredData.slice(0, ITEMS_PER_PAGE);
 
+  const handleScrollDown = () => {
+    window.scrollTo({
+      top: 1350,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <main className=" w-auto h-auto justify-items-center min-w-[300px]">
+    <main className=" w-full h-auto justify-items-center min-w-[300px]">
       <section className=" w-auto h-auto bg-secondary-700 justify-items-center">
         <Image src="/assets/images/about/intro_main.svg" alt={"3d타이포 몸체"} width={1920} height={1152} />
+        <div className="absolute inset-0 flex items-center justify-center mt-[900px]">
+          <button onClick={handleScrollDown}>
+            <Image src="/assets/images/about/scroll_down.svg" width={80} height={80} alt="스크롤 다운 버튼" />
+          </button>
+        </div>
       </section>
       <section>
-        <Image src="/assets/images/about/Rectangle1300.svg" alt={"Rectangle1300"} width={1920} height={12} />
+        <Image src="/assets/images/about/Rectangle1300.svg" alt={"이음선"} width={1920} height={12} />
       </section>
       <section className="w-auto h-auto bg-third-800 overflow-x-hidden">
         <div className="flex w-auto">
-          <Image src="/assets/images/about/Vector.svg" alt={"Vector"} width={960} height={105} />
-          <Image src="/assets/images/about/Vector.svg" alt={"Vector"} width={960} height={105} />
+          <Image src="/assets/images/about/Vector.svg" alt={"물결무늬"} width={960} height={105} />
+          <Image src="/assets/images/about/Vector.svg" alt={"물결무늬"} width={960} height={105} />
         </div>
         <div className="w-auto h-auto grid mt-[90px] place-items-center"></div>
       </section>
       <section className="w-auto h-auto overflow-x-hidden bg-third-800 flex flex-col items-center justify-center">
-        <Image src="/assets/images/about/big_frame.svg" alt={"big_frame"} width={1920} height={3855} />
-        <Link href="/location">
-          <button
-            className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-[16px] rounded-3xl relative top-[-340px]  
+        <Image src="/assets/images/about/big_frame3.svg" alt={"메인프레임"} width={1920} height={3468} />
+        <div className="grid grid-cols-4 gap-1 place-items-center absolute -mt-[2470px]">
+          <Image
+            src="/assets/images/about/horang_function_text.svg"
+            alt={"호랑이 기능을 소개합니다 텍스트"}
+            width={364}
+            height={300}
+          />
+          <Link href={"/about"}>
+            {" "}
+            <Image
+              src="/assets/images/about/about_horang_square.svg"
+              alt={"호랑소개 바로가기"}
+              width={364}
+              height={310}
+            />
+          </Link>
+          <Link href={"/travel"}>
+            {" "}
+            <Image
+              src="/assets/images/about/travel_recommend_square.svg"
+              alt={"여행지추천 바로가기"}
+              width={364}
+              height={310}
+            />
+          </Link>
+          <Link href={"location"}>
+            {" "}
+            <Image
+              src="/assets/images/about/my_near_travel_square.svg"
+              alt={"내 근처 여행지 바로가기"}
+              width={364}
+              height={310}
+            />
+          </Link>
+          <Link href={"/travelMbti"}>
+            {" "}
+            <Image
+              src="/assets/images/about/my_travel_square.svg"
+              alt={"나만의 여행 바로가기"}
+              width={364}
+              height={310}
+            />
+          </Link>
+          <Link href={"/community"}>
+            {" "}
+            <Image
+              src="/assets/images/about/horang_commu_suqare.svg"
+              alt={"호랑모임 바로가기"}
+              width={364}
+              height={310}
+            />
+          </Link>
+          <Link href={"/myspace"}>
+            {" "}
+            <Image src="/assets/images/about/my_space_square.svg" alt={"나의공간 바로가기"} width={364} height={310} />
+          </Link>
+        </div>
+
+        <div>
+          <Link href={"/travel"}>
+            <button
+              className="relative top-[-1320px] mr-[860px]  
+   "
+            >
+              <Image
+                src="/assets/images/about/introduce_travel_button.svg"
+                alt="여행지 소개"
+                width={562}
+                height={54}
+                className="object-contain"
+              />
+            </button>
+          </Link>
+        </div>
+        <div>
+          <Link href="/location">
+            <button
+              className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-[16px] rounded-3xl relative top-[-100px]  
     sm:w-[374px] sm:h-[36px] sm:text-[11px] 
     md:w-[468px] md:h-[45px] md:text-[13px]"
-          >
-            <Image
-              src="/assets/images/about/my_near_travel_button.svg"
-              alt="내 근처 여행지 찾아보기"
-              width={562}
-              height={54}
-              className="object-contain"
-            />
-          </button>
-        </Link>
+            >
+              <Image
+                src="/assets/images/about/my_near_travel_button.svg"
+                alt="내 근처 여행지 찾아보기"
+                width={562}
+                height={54}
+                className="object-contain"
+              />
+            </button>
+          </Link>
+        </div>
       </section>
       <section>
         <Image src="/assets/images/about/my_custom_travel.svg" alt={"my_custom_travel"} width={1920} height={818} />
@@ -93,22 +181,28 @@ const AboutPage = () => {
         </div>
         <div className="mt-[34px] flex items-center justify-center">
           <Link href="/community">
-            <button
-              className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-[24px] rounded-3xl 
-                     sm:w-[374px] sm:h-[36px] sm:text-[16px] 
-                     md:w-[468px] md:h-[45px] md:text-[20px]"
-            >
-              호랑모임 바로가기
-            </button>
+            <Image
+              src="/assets/images/about/horang_commu_button.svg"
+              alt="호랑모임 바로가기 버튼"
+              width={562}
+              height={54}
+              className="cursor-pointer"
+            />
           </Link>
         </div>
-        <div className="w-auto h-auto mt-[150px]">
+        <div className="w-auto h-auto mt-[155px]">
           <Image src="/assets/images/about/my_space.svg" alt={"my_space"} width={1444} height={378} />
         </div>
       </section>
       <section>
-        <div className="">
-          <Image src="/assets/images/about/about_bottom.svg" alt={"about_bottom"} width={1919} height={407} />
+        <div className="-mb-10 -mt-2">
+          <Image
+            src="/assets/images/about/about_bottom.svg"
+            alt={"about_bottom"}
+            width={1919}
+            height={407}
+            className="relative"
+          />
         </div>
       </section>
     </main>
@@ -116,14 +210,3 @@ const AboutPage = () => {
 };
 
 export default AboutPage;
-{
-  /* <Link href="/location">
-  <button
-    className="w-[562px] h-[54px] bg-primary-400 flex items-center justify-center text-white text-[16px] rounded-3xl  relative top-[-340px]  
-sm:w-[374px] sm:h-[36px] sm:text-[11px] 
-md:w-[468px] md:h-[45px] md:text-[13px]"
-  >
-    내 근처 여행지 찾아보기
-  </button>
-</Link>; */
-}
